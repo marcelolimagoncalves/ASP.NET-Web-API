c6f277709756326f2231aec755328080379cd713
Marcelo de Lima Goncalves <marcelolimagoncalves@gmail.com>
2018-01-24 00:10:40 -0200

    first commit

---
+++ ApiDevMedia/ApiDevMedia/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace ApiDevMedia
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            GlobalConfiguration.Configuration.Formatters
+             .JsonFormatter.SerializerSettings =
+              new JsonSerializerSettings
+              {
+                  Culture = CultureInfo.GetCultureInfo("pt-br"),
+                  DateFormatString = "dd/MM/yyyy"
+              };
+        }
+    }
+}
---
+++ ApiDevMedia/ApiDevMedia/Models/Curso.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ApiDevMedia.Models
+{
+    [Table("Cursos")]
+    public class Curso
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O título do curso deve ser preenchido.")]
+        [MaxLength(100, ErrorMessage = "O título do curso só pode conter até 100 caracteres.")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "A URL do curso deve ser preenchida.")]
+        [Url(ErrorMessage = "A URL do curso deve conter um endereço válido.")]
+        public string URL { get; set; }
+        [Required(ErrorMessage = "O canal do curso deve ser preenchido")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public Canal Canal { get; set; }
+        [Required(ErrorMessage = "A data de publicação do curso deve ser preenchida")]
+        public DateTime DataPublicacao { get; set; }
+        [Required(ErrorMessage = "A carga horária do curso deve ser preenchida.")]
+        [Range(1, Int32.MaxValue, ErrorMessage ="A carga horária deve ser de pelo menos 1h")]
+        public int CargaHoraria { get; set; }
+    }
+}
